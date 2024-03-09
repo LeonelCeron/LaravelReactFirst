b1fa1dbb2a8be2da0a29e40e70ad0d24a48b807b
LeonelCeron <ceronleonel99@gmail.com>
2024-03-08 23:21:02 -0600

    Sï

---
+++ app/reactfront/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+//Importar nuestros componentes
+import ShowProducts from './components/ShowProducts';
+import CreateProduct from './components/CreateProduct';
+import EditProduct from './components/EditProduct';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={ <ShowProducts />}/>
+            <Route path='/create' element={ <CreateProduct/>} />
+            <Route path='/edit/:id' element={ <EditProduct/>} />
+
+          </Routes>
+
+        </BrowserRouter>    
+    </div>
+  );
+}
+
+export default App;
